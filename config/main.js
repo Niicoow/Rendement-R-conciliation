8deac24d56dc7879d771901272b4488939a32af8
Nicolas Giboreau <nicolasgiboreau@MacBook-Air-de-Nicolas.local>
2024-05-30 12:20:04 +0200

    e

--- config/main.js
+++ config/main.js
@@ -1,1 +1,11 @@
 console.log("Bonjour !")
+
+function effacer(){
+    document.getElementById("qTa").value=""
+    document.getElementById("qTb").value=""
+    document.getElementById("qTc").value=""
+    document.getElementById("qTd").value=""
+    document.getElementById("qTe").value=""
+     
+
+}
